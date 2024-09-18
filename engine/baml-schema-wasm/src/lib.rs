dc746102a02db87dde256f182e0061f9629ed681
Nico Arqueros <1622112+nicarq@users.noreply.github.com>
2024-09-18 17:29:20 -0500

    remove wasm

--- engine/baml-schema-wasm/src/lib.rs
+++
@@ -1,9 +0,0 @@
-pub mod runtime_wasm;
-use std::env;
-use wasm_bindgen::prelude::*;
-
-#[wasm_bindgen]
-pub fn version() -> String {
-    // register_panic_hook();
-    env!("CARGO_PKG_VERSION").to_string()
-}
